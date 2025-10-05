884c926434e4c76c8a1ed4f8e503ae874c8e0bc8
Terrance <gibson.terrance.bs@outlook.com>
2025-10-04 23:10:40 -0400

    fix: updated api schema

--- api/models/categoryInfoSchema.js
+++ api/models/categoryInfoSchema.js
@@ -1,39 +1,48 @@
 import mongoose from 'mongoose';
 
 const categoryInfoSchema = new mongoose.Schema({
-  title: {
-      type: String,
-      required: true
-  },
-  abbreviatedTitle: {
-      type: String,
-      required: true
-  },
-  primaryColor: {
-      type: String,
-      required: true
-  },
-  accentColor: {
-      type: String,
-      required: true
-  },
-  description: {
-      type: String,
-      required: true
-  },
-  imgSrc: {
-      type: String,
-      required: true
-  },
-  lastUpdated: {
-      type: String,
-      required: true
-  },
-  isArchived: {
-      type: Boolean,
-      required: true,
-      default: false
-  }
+    title: {
+        type: String,
+        required: true
+    },
+    abbreviatedTitle: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    primaryColor: {
+        type: String,
+        required: true
+    },
+    accentColor: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imgSrc: {
+        type: String,
+        required: true
+    },
+    fallbackImgSrc: {
+        type: String,
+        default: ''
+    },
+    lastUpdated: {
+        type: String,
+        required: true
+    },
+    isArchived: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
 }, {
   timestamps: true,
   collection: 'categoryInfo'
